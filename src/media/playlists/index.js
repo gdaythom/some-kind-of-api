c19a96db103686621a2f05dbed114001645673ef
Thom Vincent <gday@hey.com>
2022-05-17 14:44:02 +1000

    Add new playlists.

--- src/media/playlists/index.js
+++ src/media/playlists/index.js
@@ -1,25 +1,41 @@
 const Playlists = [
-  require(`./holodecks.json`),
-  require(`./transporters.json`),
-  require(`./section31.json`),
-  require(`./theMaquisSaga.json`),
-  require(`./timeTravel.json`),
-  require(`./theWorfSaga.json`),
-  require(`./obrienMustSuffer.json`),
-  require(`./q.json`),
-  require(`./sela.json`),
-  require(`./trilogyOfTerror.json`),
-  require(`./worstEpisodes.json`),
+  require(`./multiparters/holodecks.json`),
+  require(`./multiparters/transporters.json`),
+  require(`./multiparters/section31.json`),
+  require(`./multiparters/theMaquisSaga.json`),
+  require(`./multiparters/timeTravel.json`),
+  require(`./multiparters/theWorfSaga.json`),
+  require(`./multiparters/obrienMustSuffer.json`),
+  require(`./multiparters/qAndTheEnterprise.json`),
+  require(`./multiparters/sela.json`),
+  require(`./multiparters/theBorg.json`),
+  require(`./multiparters/theFinalChapter.json`),
+  require(`./multiparters/worstEpisodes.json`),
 ];
 
 const TwoParters = [
-  require(`./twoparters/leahBrahms.json`),
-  require(`./twoparters/grilka.json`),
+  require(`./twoparters/moriarty.json`),
   require(`./twoparters/barzanWormhole.json`),
+  require(`./twoparters/leahBrahms.json`),
+  require(`./twoparters/ensignSito.json`),
+  require(`./twoparters/grilka.json`),
+  require(`./twoparters/nogsLeg.json`),
 ];
 
 const ThreeParters = [
+  require(`./threeparters/soongsAugments.json`),
+  require(`./threeparters/theSyrrannites.json`),
+  require(`./threeparters/romulanInterference.json`),
+  require(`./threeparters/theBestOfBothWorlds.json`),
   require(`./threeparters/vash.json`),
+  require(`./threeparters/theTraveler.json`),
+  require(`./threeparters/bajoranCoupDetat.json`),
+  require(`./threeparters/dominionIntroduction.json`),
+  require(`./threeparters/eddingtonVsSisko.json`),
+  require(`./threeparters/theDeathOfDax.json`),
+  require(`./threeparters/trilogyOfTerror.json`),
+  require(`./threeparters/scorpion.json`),
+  require(`./threeparters/qCivilWar.json`),
 ];
 
 export { Playlists, TwoParters, ThreeParters };
